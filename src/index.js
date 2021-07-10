062cd923bf10ebb2847f7e1e9d696544342a510f
perroentucasa <86454571+perroentucasa@users.noreply.github.com>
2021-07-10 10:22:48 +0300

    feat: change solution

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ module.exports = function check(str, bracketsConfig) {
   for (let symbStr = 0; symbStr < strArr.length; symbStr++) {
     for (let brack = 0; brack < bracketsConfig.length; brack++) {
       if (
-        strArr[symbStr] === bracketsConfig[symbStr][0] &&
-        strArr[symbStr + 1] === bracketsConfig[symbStr][1]
+        strArr[symbStr] === bracketsConfig[brack][0] &&
+        strArr[symbStr + 1] === bracketsConfig[brack][1]
       ) {
         strArr.splice(symbStr, 2);
         symbStr = -1;
